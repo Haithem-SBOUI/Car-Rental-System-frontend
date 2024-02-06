a661a4ab8a4e5358e72421839541ad79047b9ec5
Haithem-SB <haithem.sb999@gmail.com>
2024-02-06 10:12:32 +0100

    add argon theme

--- src/app/module/admin/admin-routing.module.ts
+++ src/app/module/admin/admin-routing.module.ts
@@ -2,12 +2,20 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {MainComponent} from "./main/main.component";
+import {PanelComponent} from "./panel/panel.component";
 
 const routes: Routes = [
 
   {
     path: 'dashboard',
     component: DashboardComponent,
+    children: [
+      { path: 'main', component: MainComponent },
+    ],
+  },
+  {
+    path: 'panel',
+    component: PanelComponent,
     children: [
       { path: 'main', component: MainComponent },
     ],
--- src/app/module/admin/admin.module.ts
+++ src/app/module/admin/admin.module.ts
@@ -5,12 +5,14 @@ import {AdminRoutingModule} from './admin-routing.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {FormsModule} from "@angular/forms";
 import {MainComponent} from './main/main.component';
+import { PanelComponent } from './panel/panel.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    MainComponent
+    MainComponent,
+    PanelComponent
   ],
   imports: [
     CommonModule,
